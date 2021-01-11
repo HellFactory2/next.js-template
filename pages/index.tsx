a6b788ecea9a858701529f2de33c4d19988b08d6
nicscthongchai <nicscthongchai@gmail.com>
2021-01-11 11:51:32 +0700

    update index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,10 +3,10 @@ import { NextPage } from 'next';
 const IndexPage: NextPage = () => {
   return (
     <main>
-      <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
-        <div className="px-4 py-6 sm:px-0">
-          <div className="border-4 border-dashed border-gray-200 rounded-lg h-96 flex justify-center items-center text-lg">
-            Good luck and have fun
+      <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8 h-screen">
+        <div className="px-4 py-6 sm:px-0 h-full">
+          <div className="border-4 border-dashed border-gray-200 rounded-lg h-full flex justify-center items-center text-lg">
+            Happy Coding!
           </div>
         </div>
       </div>
