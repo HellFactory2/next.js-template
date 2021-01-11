a3521d8692cc18ac8edabcee1ea4b62d433bb1bf
nicscthongchai <nicscthongchai@gmail.com>
2021-01-11 12:03:28 +0700

    update tailwind breakpoint

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@ module.exports = {
   purge: ['./pages/**/*.tsx', './components/**/*.tsx'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    screens: {
+      sm: { min: '640px', max: '767px' },
+      md: { min: '768px', max: '1023px' },
+      lg: { min: '1024px', max: '1279px' },
+      xl: { min: '1280px', max: '1535px' },
+      xxl: { min: '1536px' },
+    },
     extend: {},
   },
   variants: {
