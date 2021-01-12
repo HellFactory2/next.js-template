1c9d28262d22b842d67b36456ead56b20d0abcea
nicscthongchai <nicscthongchai@gmail.com>
2021-01-12 09:10:25 +0700

    try dark mode

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,9 +3,9 @@ import { NextPage } from 'next';
 const IndexPage: NextPage = () => {
   return (
     <main>
-      <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8 h-screen">
+      <div className="bg-white dark:bg-gray-800 max-w-screen mx-auto py-6 sm:px-6 lg:px-8 h-screen">
         <div className="px-4 py-6 sm:px-0 h-full">
-          <div className="border-4 border-dashed border-gray-200 rounded-lg h-full flex justify-center items-center text-lg">
+          <div className="border-4 border-dashed border-gray-200 dark:white rounded-lg h-full flex justify-center items-center text-lg">
             Happy Coding!
           </div>
         </div>
