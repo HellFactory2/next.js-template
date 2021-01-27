356d0f4be438c541f25a9022c2d3fdbffc164a9d
nicscthongchai <nicscthongchai@gmail.com>
2021-01-27 21:07:17 +0700

    update proxy setting

--- server/use-proxy.ts
+++ server/use-proxy.ts
@@ -1,17 +1,31 @@
 import { createProxyMiddleware, Options } from 'http-proxy-middleware';
 import { Express } from 'express';
 
+const { API_URL, API_URLs } = process.env;
+
 interface UseProxyConfig {
   [path: string]: Options;
 }
 
 const config: UseProxyConfig = {
-  '/api/': {
-    target: process.env.API_URL || '',
+  '/api': {
+    target: API_URL || '',
     pathRewrite: { '^/api': '' },
     changeOrigin: true,
   },
 };
+
+if (API_URLs) {
+  const urls = JSON.parse(API_URLs || '');
+  for (const path of Object.keys(urls)) {
+    const target = urls[path];
+    config[path] = {
+      target,
+      pathRewrite: { [`^${path}`]: '' },
+      changeOrigin: true,
+    };
+  }
+}
 
 export default function (server: Express) {
   for (const path of Object.keys(config)) {
