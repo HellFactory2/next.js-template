69c14188914762721a6f8a8882665030745dc6c0
nicscthongchai <nicscthongchai@gmail.com>
2021-02-15 21:14:47 +0700

    update _app & _document page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,9 @@ import 'styles/global.scss';
 
 import { NextComponentType } from 'next';
 import { AppContext, AppInitialProps, AppProps } from 'next/app';
-import Head from 'next/head';
 import Router from 'next/router';
 import nprogress from 'nprogress';
 import { RecoilRoot } from 'recoil';
-import Title from 'components/Layout/Title';
 
 Router.events.on('routeChangeStart', () => {
   nprogress.start();
@@ -24,19 +22,9 @@ Router.events.on('routeChangeComplete', () => {
 const _App: NextComponentType<AppContext, AppInitialProps, AppProps> = props => {
   const { Component, pageProps } = props;
   return (
-    <>
-      <Title></Title>
-      <Head>
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Prompt:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
-      <RecoilRoot>
-        <Component {...pageProps} />
-      </RecoilRoot>
-    </>
+    <RecoilRoot>
+      <Component {...pageProps} />
+    </RecoilRoot>
   );
 };
 
