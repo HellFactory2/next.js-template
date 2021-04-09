980b23d83c1e31611891c042d8cc1ce27696b839
nicscthongchai <nicscthongchai@gmail.com>
2021-04-09 10:21:35 +0700

    removed styled components package

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,7 +8,6 @@ import Document, {
   DocumentInitialProps,
   DocumentProps,
 } from 'next/document';
-import { ServerStyleSheet } from 'styled-components';
 
 const _Document: NextComponentType<DocumentContext, DocumentInitialProps, DocumentProps> & {
   renderDocument: Function;
@@ -31,28 +30,11 @@ const _Document: NextComponentType<DocumentContext, DocumentInitialProps, Docume
 };
 
 _Document.getInitialProps = async ctx => {
-  const sheet = new ServerStyleSheet();
-  const originalRenderPage = ctx.renderPage;
-
-  try {
-    ctx.renderPage = () =>
-      originalRenderPage({
-        enhanceApp: (App: any) => (props: any) => sheet.collectStyles(<App {...props} />),
-      });
-
-    const initialProps = await Document.getInitialProps(ctx);
-    return {
-      ...initialProps,
-      styles: (
-        <>
-          {initialProps.styles}
-          {sheet.getStyleElement()}
-        </>
-      ),
-    };
-  } finally {
-    sheet.seal();
-  }
+  const initialProps = await Document.getInitialProps(ctx);
+  return {
+    ...initialProps,
+    styles: <>{initialProps.styles}</>,
+  };
 };
 
 _Document.renderDocument = Document.renderDocument;
